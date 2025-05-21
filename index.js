5b4e1f6b700eab0302c706dcb13dd2034c69f50d
Yhasmin-Barros <yhasminbarros1@gmail.com>
2025-05-21 17:53:29 +0000

    aula-2105

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'hbs');
+app.set('views', './views');
+
+app.get('/', (request, response)=>{
+    let resultado = []
+    let n1 = 0
+    let n2 = 1
+
+    for(let i = 0 ; i<=20; i++){
+        resultado.push(n1)
+        let temp = n1 + n2;
+        n1 = n2;
+        n2 =temp;
+    }
+    response.render('home', {valores: resultado})
+})
+app.listen(3000, () =>{
+    console.log('Servidor na porta 3000')
+})
